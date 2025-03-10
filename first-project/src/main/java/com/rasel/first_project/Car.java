64a8af234f3264705fb9decb99fba8eeebf3833c
mdrasel202 <raselabc204@gmail.com>
2025-03-10 23:21:15 +0600

    done

---
+++ first-project/src/main/java/com/rasel/first_project/Car.java
@@ -0,0 +1,17 @@
+package com.rasel.first_project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Car {
+    @Autowired
+    private Dog dog;
+    //private Dog dog = new Dog();
+
+    @GetMapping("/ok")
+        public String ok(){
+        return dog.fun();
+    }
+}
---
+++ first-project/src/main/java/com/rasel/first_project/Dog.java
@@ -0,0 +1,10 @@
+package com.rasel.first_project;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+    public String fun(){
+        return "Something";
+    }
+}
