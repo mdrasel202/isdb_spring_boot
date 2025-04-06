4a74b6f3dae3d1c9c55ed175683f4af019849a23
mdrasel202 <raselabc204@gmail.com>
2025-04-06 19:47:29 +0600

    done

---
+++ productApp/src/main/java/com/example/second_project/model/Product.java
@@ -0,0 +1,16 @@
+package com.example.second_project.model;
+
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releseDate;
+    private boolean
+}
