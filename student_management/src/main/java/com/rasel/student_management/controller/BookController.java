a47136fcf7e2de251cf8bedd197289d39eabe1b2
mdrasel202 <raselabc204@gmail.com>
2025-04-07 13:10:41 +0600

    done

--- student_management/src/main/java/com/rasel/student_management/controller/BookController.java
+++ student_management/src/main/java/com/rasel/student_management/controller/BookController.java
@@ -1,56 +1,69 @@
 package com.rasel.student_management.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.rasel.student_management.dto.BookDTO;
 import com.rasel.student_management.model.Book;
 import com.rasel.student_management.service.BookService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping(value = "/book")
+@Tag(name = "Book Controller", description = "API for management")
 public class BookController {
-    private final BookService bookService;
+	private final BookService bookService;
 
-    public BookController(BookService bookService){
-        this.bookService = bookService;
-    }
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+	}
 
-    //post
-    @PostMapping
-    public ResponseEntity<Book> saveBook(@RequestBody BookDTO bookDTO){
-        Book saved = bookService.saveBook(bookDTO);
-        return new ResponseEntity<>(saved, HttpStatus.CREATED);
-    }
+	// post
+	@PostMapping
+	@Operation(summary = "Save book to DB", description = "Save book to DB description")
+	public ResponseEntity<Book> saveBook(@RequestBody BookDTO bookDTO) {
+		Book saved = bookService.saveBook(bookDTO);
+		return new ResponseEntity<>(saved, HttpStatus.CREATED);
+	}
 
-    //get
-    @GetMapping("/{id}")
-    public ResponseEntity<Book> getBook(@PathVariable Integer id){
-        Book book = bookService.getBook(id);
-        return new ResponseEntity<>(book, HttpStatus.OK);
-    }
+	// get
+	@GetMapping("/{id}")
+	public ResponseEntity<Book> getBook(@PathVariable Integer id) {
+		Book book = bookService.getBook(id);
+		return new ResponseEntity<>(book, HttpStatus.OK);
+	}
 
-    //delete
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteBook(@PathVariable Integer id){
-        bookService.deleteBook(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+	// delete
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteBook(@PathVariable Integer id) {
+		bookService.deleteBook(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 
-    //get all
-    @GetMapping
-    public ResponseEntity<List<Book>> getAllBook(){
-        List<Book> books = bookService.getAllBook();
-        return new ResponseEntity<>(books, HttpStatus.OK);
-    }
+	// get all
+	@GetMapping
+	public ResponseEntity<List<Book>> getAllBook() {
+		List<Book> books = bookService.getAllBook();
+		return new ResponseEntity<>(books, HttpStatus.OK);
+	}
 
-    //put
-    @PutMapping("/{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable Integer id, @RequestBody Book book){
-        Book updated = bookService.updateBook(id, book);
-        return new ResponseEntity<>(updated, HttpStatus.OK);
-    }
+	// put
+	@PutMapping("/{id}")
+	public ResponseEntity<Book> updateBook(@PathVariable Integer id, @RequestBody Book book) {
+		Book updated = bookService.updateBook(id, book);
+		return new ResponseEntity<>(updated, HttpStatus.OK);
+	}
 
 }
--- student_management/src/main/java/com/rasel/student_management/controller/StudentController.java
+++ student_management/src/main/java/com/rasel/student_management/controller/StudentController.java
@@ -2,7 +2,6 @@ package com.rasel.student_management.controller;
 
 import java.util.List;
 
-import com.rasel.student_management.dto.StudentDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,51 +13,55 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rasel.student_management.dto.StudentDTO;
 import com.rasel.student_management.model.Student;
 import com.rasel.student_management.service.StudentService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(value = "/student")
+@Tag(name = "Student Controller", description = "API for management")
 public class StudentController {
-    
-    private final StudentService studentService;
 
-    public StudentController(StudentService studentService){
-        this.studentService = studentService;
-    }
+	private final StudentService studentService;
 
-    //post
-    @PostMapping
-    public ResponseEntity<Student> saveStudent(@RequestBody StudentDTO studentDTO){
-        Student saved = studentService.saveStudent(studentDTO);
-        return new ResponseEntity<>(saved, HttpStatus.CREATED);
-    }
+	public StudentController(StudentService studentService) {
+		this.studentService = studentService;
+	}
 
-    //get
-    @GetMapping("/{id}")
-    public ResponseEntity<Student> getStudent(@PathVariable Integer id){
-        Student student = studentService.getStudent(id);
-        return new ResponseEntity<>(student, HttpStatus.OK);
-    }
+	// post
+	@PostMapping
+	public ResponseEntity<Student> saveStudent(@RequestBody StudentDTO studentDTO) {
+		Student saved = studentService.saveStudent(studentDTO);
+		return new ResponseEntity<>(saved, HttpStatus.CREATED);
+	}
 
-    //delete
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Student> deleteStudent(@PathVariable Integer id){
-        studentService.deleteStudent(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+	// get
+	@GetMapping("/{id}")
+	public ResponseEntity<Student> getStudent(@PathVariable Integer id) {
+		Student student = studentService.getStudent(id);
+		return new ResponseEntity<>(student, HttpStatus.OK);
+	}
 
-    //get All Student
-    @GetMapping
-    public ResponseEntity<List<Student>> getAllStudent(){
-        List<Student> students = studentService.getAllStudent();
-        return new ResponseEntity<>(students, HttpStatus.OK);
-    }
+	// delete
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Student> deleteStudent(@PathVariable Integer id) {
+		studentService.deleteStudent(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 
-    //put
-    @PutMapping("/{id}")
-    public ResponseEntity<Student> updaStudent(@PathVariable Integer id, @RequestBody StudentDTO studentDTO){
-        Student updated = studentService.updaStudent(id, studentDTO);
-        return new ResponseEntity<>(updated, HttpStatus.OK);
-    }
+	// get All Student
+	@GetMapping
+	public ResponseEntity<List<Student>> getAllStudent() {
+		List<Student> students = studentService.getAllStudent();
+		return new ResponseEntity<>(students, HttpStatus.OK);
+	}
+
+	// put
+	@PutMapping("/{id}")
+	public ResponseEntity<Student> updaStudent(@PathVariable Integer id, @RequestBody StudentDTO studentDTO) {
+		Student updated = studentService.updaStudent(id, studentDTO);
+		return new ResponseEntity<>(updated, HttpStatus.OK);
+	}
 }
